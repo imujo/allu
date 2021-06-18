79c1113aca91decad5cb264bc8ec2ebb6cb607cb
Ivo Mujo <ivo.mujo.3@gmail.com>
2021-06-18 11:35:40 +0200

    adding custom scrollbars

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import {Switch, Route} from 'react-router-dom'
 import Home from './Components/Home/1-Home'
 import ArticlePage from './Components/ArticlePage/1-ArticlePage'
 
+
 function App() {
   return (
     <div className="app">
-      <Switch>
-        <Route path='/article'>
-          <ArticlePage />
-        </Route>
-        <Route path='/'>
-          <Home />
-        </Route>
-      </Switch>
+      
+        <Switch>
+          <Route path='/article'>
+            <ArticlePage />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
     </div>
   );
 }
--- src/Components/ArticlePage/4-Comments.js
+++ src/Components/ArticlePage/4-Comments.js
@@ -1,27 +1,34 @@
 import React from 'react'
+import { Scrollbars } from 'react-custom-scrollbars';
+
 
 function Comments() {
     return (
-    <div className='commentsDiv' >
+        <div className='commentsDiv' >
             <div className='commentTitleFont'>Comments</div>
-            <div className="commentsList">
-                {
-                    [1,1,1,1,1,1,1].map((value, i)=>{
-                        return (
-                            <div className="comment">
-                            <div className="userIcon commentTitleFont">A</div>
-                            <div className="commentText commentFont">This is great</div>
-                        </div>
-                        )
-                    })
-                }
-               
+            <Scrollbars style={{ width: "100%", height: "100%" }}>
 
-            </div>
+                <div className="commentsList">
+                    {
+                        [1,1,1,1,1,1,1].map((value, i)=>{
+                            return (
+                                <div key={i} className="comment">
+                                    <div className="userIcon commentTitleFont">A</div>
+                                    <div className="commentText commentFont">This is great</div>
+                                </div>
+                            )
+                        })
+                    }
+                    
+
+                </div>
+            </Scrollbars>
+
             <div className='center'>
                 <input type='text' className="leaveComment" placeholder='Comment'></input>
             </div>
         </div>
+   
     )
 }
 
